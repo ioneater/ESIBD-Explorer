4d62fa58799b50b5a42f3d0fb157bdb843548ac3
ioneater <tim.esser@gmx.de>
2023-10-31 21:06:23 +0000

    fixed typo

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,11 @@ print(f'Running using {sys.executable}') # validate that we are running in corre
 
 docs_path = os.path.dirname(os.path.abspath(__file__))
 print('docs_path: ', docs_path)
-package_path = os.path.join(path, '..')
+package_path = os.path.join(docs_path, '..')
 print('package_path: ', package_path)
 print('walk package_path: ', [x[0] for x in os.walk(package_path)])
 sys.path.insert(0, package_path)
-sys.path.insert(0, os.path.join(path, '..', '..'))
+sys.path.insert(0, os.path.join(docs_path, '..', '..'))
 print('sys.path: ',sys.path)
 import esibd
 
